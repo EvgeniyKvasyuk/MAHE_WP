a8398a44d1787ac306ca0952f15a78c847e260bb
Vladislav Solodiankin <Vladislav_Solodiankin@epam.com>
2022-04-08 14:39:36 +0300

    feat: [PT-22] base component polishing

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 
 import DividerMaterial, { DividerProps as DividerPropsMaterial } from '@mui/material/Divider';
+import classNames from 'classnames/bind';
+
+import styles from './Divider.module.css';
 
 export type DividerProps = Pick<DividerPropsMaterial, 'className' | 'style'>;
 
+const cn = classNames.bind(styles);
+
 export function Divider({ className, style }: DividerProps) {
-  return <DividerMaterial style={style} className={className} />;
+  return <DividerMaterial style={style} className={cn('divider', className)} />;
 }
--- src/components/Menu/components/MenuItem.tsx
+++ src/components/Menu/components/MenuItem.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 
+import Icon from '@mui/material/Icon';
 import MenuItemMaterial, { MenuItemProps } from '@mui/material/MenuItem';
+import classNames from 'classnames/bind';
+
+import styles from './MenuItem.module.css';
 
 export type MenuProps = {
   className?: string;
+  noHover?: boolean;
+  leftIcon?: string;
 } & Pick<MenuItemProps, 'children' | 'autoFocus' | 'selected' | 'onClick'>;
 
-export function MenuItem({ autoFocus, children, className, onClick, selected }: MenuProps) {
+const cn = classNames.bind(styles);
+
+export function MenuItem({ autoFocus, children, className, onClick, selected, noHover, leftIcon }: MenuProps) {
   return (
-    <MenuItemMaterial autoFocus={autoFocus} className={className} onClick={onClick} selected={selected}>
+    <MenuItemMaterial
+      autoFocus={autoFocus}
+      className={cn('menu-item', className, {
+        'menu-item--no-hover': noHover,
+      })}
+      onClick={onClick}
+      selected={selected}
+    >
+      {leftIcon && <Icon className={cn('menu-item__icon')}>{leftIcon}</Icon>}
       {children}
     </MenuItemMaterial>
   );
